9b9374ec2293f8c32d99703182a04a1bca1bb1bc
jatindra <jatindrobanerjee@gmail.com>
2024-09-11 17:56:57 +0530

    many have change

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '../src/route/route'
-createApp(App).use(router).mount('#app')
+import PrimeVue from 'primevue/config';
+
+import '/src/assets/styles.scss';
+import Ripple from 'primevue/ripple';
+
+// Define the app before using it
+const app = createApp(App);
+
+app.use(router);
+app.use(PrimeVue, { ripple: true });
+app.directive('ripple', Ripple); // Add Ripple directive
+app.mount('#app');
